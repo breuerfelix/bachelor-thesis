9597447313a05003f6694738b7b671d3c343cea3
Felix Breuer <fbreuer@pm.me>
2021-02-24 12:46:00 +0100

    substitude german umlaute
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# clean output
 rm main.pdf
+
+# generate syntax highlighted snippets
 rm -rf gen
 python minted.py
+
+# replace german umlaute
+sed -i 's/ß/{\\ss}/g' content/*
+
+# compile latex
 tectonic main.tex
