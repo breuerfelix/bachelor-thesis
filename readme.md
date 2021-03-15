e7b9d30f07c765366edc74a02c9949304aa5ada8
Felix Breuer <fbreuer@pm.me>
2021-03-15 16:11:44 +0100

    overload protection
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 ## wichtige formale fragen
 
+- unterscheidung hivemq node / cluster und envoy node / cluster
 - müssen wörter wie subscriber, publisher, payload, header etc übersetzt werden ?
 - endet ein absatz wenn eine liste anfängt ? also muss ich bevor die liste anfängt den quellenverweis schreiben?
 - wie schreibe ich: mehr infos zu den optionalen feldern gibt es hier: quelle
--- snippets/prometheus-curl.sh
+++ snippets/prometheus-curl.sh
@@ -1,5 +1,7 @@
 curl -s http://localhost:9399/metrics | grep cpu_total_total
 
-# HELP com_hivemq_system_os_global_cpu_total_total Generated from Dropwizard metric import (metric=com.hivemq.system.os.global.cpu.total.total, type=eO.h)
+# HELP com_hivemq_system_os_global_cpu_total_total Generated from \
+# Dropwizard metric import \
+# (metric=com.hivemq.system.os.global.cpu.total.total, type=eO.h)
 # TYPE com_hivemq_system_os_global_cpu_total_total gauge
 com_hivemq_system_os_global_cpu_total_total 5.0
